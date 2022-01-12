d603ea132c9f6c5650599174cf324728492bd01c
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2022-01-11 21:12:44 -0500

    BPB: Fix broken import

--- BPBungee/src/main/java/net/bridgepractice/bpbungee/BPBungee.java
+++ BPBungee/src/main/java/net/bridgepractice/bpbungee/BPBungee.java
@@ -8,6 +8,7 @@ import net.luckperms.api.model.user.User;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
