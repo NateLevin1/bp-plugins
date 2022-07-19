199144998a7118a343db1a73d708f9aafff60d14
Bludenz <97531671+Bludenz@users.noreply.github.com>
2022-07-19 10:27:44 +0800

    Changed

--- BridgePracticeLobby/src/main/java/net/bridgepractice/BridgePracticeLobby/EnderbuttGrant.java
+++ BridgePracticeLobby/src/main/java/net/bridgepractice/BridgePracticeLobby/EnderbuttGrant.java
@@ -14,50 +14,25 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
 public class EnderbuttGrant implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-
-            if (p.hasPermission("group.admin")) {
-
-                if (args.length == 0) {
-
-                    ItemStack EnderButt = Utils.createEnderButt();
-                    p.getInventory().addItem(EnderButt);
-
-                    p.sendMessage(ChatColor.GREEN + "You have given yourself an EnderButt!");
-
-                } else {
-                    Player target = Bukkit.getPlayerExact(args[0]);
-
-                    if (target == null) {
-                        p.sendMessage(ChatColor.RED + "This player doesn't exist");
-
-                        return true;
-                    }
-
-                    ItemStack EnderButt = Utils.createEnderButt();
-                    target.getInventory().addItem(EnderButt);
-
-                    target.sendMessage(ChatColor.GREEN + "You have been given an EnderButt by " + p.getDisplayName());
-
-                }
-
-            }else{
-                p.sendMessage(ChatColor.RED + "You do not have permission!");
-            }
-
+        if (!(sender instanceof Player)) return true;
+        Player player = ((Player) sender);
+        BridgePracticeLobby.Gadget current = BridgePracticeLobby.instance.getGadget(player);
+        if (current.fifthSlotItem.getType() == Material.ENDER_PEARL) {
+            // unrod
+            BridgePracticeLobby.instance.setGadget(player, null);
+            BridgePracticeLobby.instance.giveGadgets(player, player.getInventory());
+        } else {
+            giveGadget(player);
         }
-
         return true;
     }
 
-    public static void giveGadget(Player player) {
-        BridgePracticeLobby.Gadget current = BridgePracticeLobby.instance.getGadget(player);
-        ItemStack enderbutt = Utils.makeItem(Material.ENDER_PEARL, "§5EnderButt", "§7Shoot your shot and", "§7teleport anywhere!");
-        BridgePracticeLobby.instance.setGadget(player, new BridgePracticeLobby.Gadget(current.fourthSlotItem, enderbutt));
-        player.getInventory().setHeldItemSlot(5);
-        BridgePracticeLobby.instance.giveGadgets(player, player.getInventory());
+        public static void giveGadget(Player p){
+            BridgePracticeLobby.Gadget current = BridgePracticeLobby.instance.getGadget(p);
+            ItemStack enderbutt = Utils.makeItem(Material.ENDER_PEARL, "§5EnderButt", "§7Shoot your shot and", "§7teleport anywhere!");
+            BridgePracticeLobby.instance.setGadget(p, new BridgePracticeLobby.Gadget(current.fourthSlotItem, enderbutt));
+            p.getInventory().setHeldItemSlot(5);
+            BridgePracticeLobby.instance.giveGadgets(p, p.getInventory());
 
+        }
     }
-}
