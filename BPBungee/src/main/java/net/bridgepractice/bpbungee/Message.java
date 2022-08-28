b7677abf7a4d55a593c949362b82cca844f61f8f
Bludenz <97531671+Bludenz@users.noreply.github.com>
2022-08-28 17:09:21 +0800

    Add return statement

--- BPBungee/src/main/java/net/bridgepractice/bpbungee/Message.java
+++ BPBungee/src/main/java/net/bridgepractice/bpbungee/Message.java
@@ -29,6 +29,7 @@ public class Message extends Command {
         String playerName = args[0];
         if(playerName.equalsIgnoreCase(sender.getName())) {
             sender.sendMessage(new ComponentBuilder("You cannot message yourself!").color(ChatColor.RED).create());
+            return;
         }
         String text = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
         ProxiedPlayer player = BPBungee.instance.getProxy().getPlayer(playerName);
