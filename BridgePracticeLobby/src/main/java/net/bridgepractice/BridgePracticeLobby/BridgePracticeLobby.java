7d6f86649d13e263e0a71b8690f0072c78ec82ee
Bludenz <97531671+Bludenz@users.noreply.github.com>
2022-10-11 23:09:24 +0800

    BPL: Fix bugs with pets (#34)
    * Fix being able to "steal pets"
    
    * Fix sheep pet not showing players name

--- BridgePracticeLobby/src/main/java/net/bridgepractice/BridgePracticeLobby/BridgePracticeLobby.java
+++ BridgePracticeLobby/src/main/java/net/bridgepractice/BridgePracticeLobby/BridgePracticeLobby.java
@@ -900,6 +900,7 @@ public class BridgePracticeLobby extends JavaPlugin implements Listener, PluginM
         }
         if (type == PetType.RAINBOW_SHEEP) {
             Sheep sheep = (Sheep) p.getWorld().spawn(p.getLocation(), animal);
+            sheep.setCustomName(petName);
             rainbowSheep.put(p.getUniqueId(), new BukkitRunnable() {
 
                 int dyeIndex = 0;
@@ -1513,6 +1514,8 @@ public class BridgePracticeLobby extends JavaPlugin implements Listener, PluginM
         if (event.getRightClicked().getType() == EntityType.PLAYER && event.getPlayer().getItemInHand().getType() == Material.LEASH) {
            Player target = (Player) event.getRightClicked();
            target.setPassenger(p);
+        } else {
+            event.setCancelled(true);
         }
     }
     @EventHandler
